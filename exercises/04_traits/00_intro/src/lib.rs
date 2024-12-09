84e4e2e64c88f94bff7427f9724e2034f544b79d
iuxtaa <140783998+iuxtaa@users.noreply.github.com>
2024-12-09 15:00:27 +1300

    feat: finish traits and ticket_v2 (#3)

--- exercises/04_traits/00_intro/src/lib.rs
+++ exercises/04_traits/00_intro/src/lib.rs
@@ -1,6 +1,16 @@
+// Trait bounds
+
+/*
+    Use cases:
+    1) Unlocking 'build-in' behaviour (e.g. operator overloading)
+    2) Adding new behaviour to existing types (i.e. extension traits)
+    3) Generic programming
+
+*/
+
 fn intro() -> &'static str {
     // TODO: fix me 👇
-    "I'm ready to __!"
+    "I'm ready to learn about traits!"
 }
 
 #[cfg(test)]
